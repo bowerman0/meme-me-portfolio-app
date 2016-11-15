5ec495f728d2f04dcffff8e863484c91eba6fb28
Michael Bowerman <michael.bowerman@gmail.com>
2016-11-14 18:11:45 -0800

    Add label and button without storyboard.

--- meme-me/meme-me/ViewController.swift
+++ meme-me/meme-me/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Label
+        var label = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        // Button
+        var button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
