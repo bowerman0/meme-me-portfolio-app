13cfb823b41bc15fd1749b6ca2458d07ba52ddd3
Michael Bowerman <michael.bowerman@gmail.com>
2016-11-15 21:21:32 -0800

    hook up button to label.

--- meme-me/meme-me/ViewController.swift
+++ meme-me/meme-me/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +22,7 @@ class ViewController: UIViewController {
         label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         self.view.addSubview(label)
+        self.label = label
         
         // Button
         var button = UIButton()
@@ -26,13 +30,18 @@ class ViewController: UIViewController {
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(incrementCount), for: UIControlEvents.touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func incrementCount() {
+        self.count = self.count+1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
