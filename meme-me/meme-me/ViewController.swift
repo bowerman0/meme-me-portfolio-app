1f2ed4ec27db9ea05e7f6f325ab8c1aba2b37e6c
Michael Bowerman <michael.bowerman@gmail.com>
2016-12-04 13:32:11 -0800

    Text field cleanup.

--- meme-me/meme-me/ViewController.swift
+++ meme-me/meme-me/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     // MARK: Outlets
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
 
     // MARK: Actions
 
@@ -45,13 +47,25 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
 
+    // MARK: UITextView
+
+    @IBAction func textEditingDidBegin(_ sender: Any) {
+        if let textField = sender as? UITextField {
+            textField.placeholder = nil
+        }
+    }
+    func textFieldShouldReturn(_ textField:UITextField)->Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
     // MARK: UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        self.topText.delegate = self
+        self.bottomText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
